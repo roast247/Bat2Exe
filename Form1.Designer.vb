22a672e77f4cd603cac731fa7e05bea84a2dfd74
roast247 <77567356+roast247@users.noreply.github.com>
2021-03-05 14:33:08 -0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,323 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.Button2 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.Button3 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.Button4 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.closeApp = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.KryptonLinkLabel1 = New ComponentFactory.Krypton.Toolkit.KryptonLinkLabel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(343, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.Button1.Size = New System.Drawing.Size(29, 25)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Values.Text = "..."
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Location = New System.Drawing.Point(12, 291)
+        Me.Button2.Name = "Button2"
+        Me.Button2.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.Button2.Size = New System.Drawing.Size(113, 25)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Values.Text = "Convert"
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Location = New System.Drawing.Point(12, 31)
+        Me.Button3.Name = "Button3"
+        Me.Button3.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.Button3.Size = New System.Drawing.Size(100, 25)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Values.Text = "Use text instead"
+        '
+        'Button4
+        '
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Location = New System.Drawing.Point(291, 223)
+        Me.Button4.Name = "Button4"
+        Me.Button4.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.Button4.Size = New System.Drawing.Size(29, 25)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Values.Text = "..."
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(206, 31)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(131, 25)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(288, 265)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Status: Inactive"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(118, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Batch File Path:"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.closeApp)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(383, 25)
+        Me.Panel1.TabIndex = 7
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Roasting_Bat_to_Exe.My.Resources.Resources.bat2exe
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(26, 25)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 16
+        Me.PictureBox2.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(32, 6)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(101, 13)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Roasting Bat to Exe"
+        '
+        'closeApp
+        '
+        Me.closeApp.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.closeApp.Dock = System.Windows.Forms.DockStyle.Right
+        Me.closeApp.Location = New System.Drawing.Point(358, 0)
+        Me.closeApp.Name = "closeApp"
+        Me.closeApp.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.closeApp.Size = New System.Drawing.Size(25, 25)
+        Me.closeApp.StateNormal.Back.Color1 = System.Drawing.Color.Red
+        Me.closeApp.StateNormal.Back.Color2 = System.Drawing.Color.Black
+        Me.closeApp.StatePressed.Back.Color1 = System.Drawing.Color.Maroon
+        Me.closeApp.StatePressed.Back.Color2 = System.Drawing.Color.Black
+        Me.closeApp.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.closeApp.StateTracking.Back.Color2 = System.Drawing.Color.Black
+        Me.closeApp.TabIndex = 0
+        Me.closeApp.Values.Text = "X"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.Color.White
+        Me.TextBox2.Location = New System.Drawing.Point(121, 260)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(164, 25)
+        Me.TextBox2.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 265)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Executable File Path:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox4.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.ForeColor = System.Drawing.Color.White
+        Me.TextBox4.Location = New System.Drawing.Point(90, 223)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(195, 25)
+        Me.TextBox4.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 228)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(75, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Icon File Path:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.ForeColor = System.Drawing.Color.White
+        Me.TextBox3.Location = New System.Drawing.Point(50, 70)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox3.Size = New System.Drawing.Size(307, 140)
+        Me.TextBox3.TabIndex = 5
+        Me.TextBox3.Text = "-ping 172.0.0.2"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 75)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Code:"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ProgressBar1.ForeColor = System.Drawing.Color.Navy
+        Me.ProgressBar1.Location = New System.Drawing.Point(147, 291)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(138, 25)
+        Me.ProgressBar1.Step = 5
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 14
+        '
+        'KryptonLinkLabel1
+        '
+        Me.KryptonLinkLabel1.Location = New System.Drawing.Point(305, 299)
+        Me.KryptonLinkLabel1.Name = "KryptonLinkLabel1"
+        Me.KryptonLinkLabel1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.KryptonLinkLabel1.Size = New System.Drawing.Size(64, 17)
+        Me.KryptonLinkLabel1.StateCommon.ShortText.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.KryptonLinkLabel1.TabIndex = 16
+        Me.KryptonLinkLabel1.Values.Text = "README"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(326, 218)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(35, 35)
+        Me.PictureBox1.TabIndex = 15
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(383, 328)
+        Me.Controls.Add(Me.KryptonLinkLabel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(383, 328)
+        Me.MinimumSize = New System.Drawing.Size(383, 328)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents Button2 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents Button3 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents Button4 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents closeApp As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents KryptonLinkLabel1 As ComponentFactory.Krypton.Toolkit.KryptonLinkLabel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,240 @@
+﻿Imports System.CodeDom.Compiler
+Public Class Form1
+    Dim fichier, icone As String
+    Dim Source As String =
+    "Module BtE" & vbNewLine &
+    "Sub Main()" & vbNewLine &
+    "If System.IO.File.Exists(System.Environment.GetEnvironmentVariable(" & Chr(34) & "TMP" & Chr(34) & ") & " & Chr(34) & "\cmd.bat" & Chr(34) & ") Then" & vbNewLine &
+    "System.IO.File.Delete(System.Environment.GetEnvironmentVariable(" & Chr(34) & "TMP" & Chr(34) & ") & " & Chr(34) & "\cmd.bat" & Chr(34) & ")" & vbNewLine &
+    "End If" & vbNewLine &
+    "System.IO.File.WriteAllText(System.Environment.GetEnvironmentVariable(" & Chr(34) & "TMP" & Chr(34) & ") & " & Chr(34) & "\cmd.bat" & Chr(34) & ", {BATCH})" & vbNewLine &
+    "System.Diagnostics.Process.Start(System.Environment.GetEnvironmentVariable(" & Chr(34) & "TMP" & Chr(34) & ") & " & Chr(34) & "\cmd.bat" & Chr(34) & ")" & vbNewLine &
+    "End Sub" & vbNewLine &
+    "End Module"
+
+    Private draggable As Boolean
+    Private mouseY As Integer
+    Private mouseX As Integer
+    Private drag As Boolean
+
+    Public Sub GenerateExecutable(ByVal Output As String, ByVal Source As String)
+        Try
+            Dim Param As New CompilerParameters()
+            Dim Res As CompilerResults
+            Param.GenerateExecutable = True
+            Param.IncludeDebugInformation = False
+            Param.CompilerOptions = "/target:winexe /optimize+ /filealign:512"
+            Param.OutputAssembly = Output
+            Res = New VBCodeProvider(New Dictionary(Of String, String)() From {{"Version", "v2"}}).CompileAssemblyFromSource(Param, Source)
+            If Res.Errors.Count <> 0 Then
+                For Each [Error] In Res.Errors
+                    MessageBox.Show("Error: " & [Error].ErrorText, "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Next
+            End If
+        Catch ex As Exception
+            MsgBox("Error: " & ex.ToString)
+        End Try
+    End Sub
+    Function TobString(ByVal b As String)
+        Dim ret As String = ""
+        For i As Integer = 0 To Split(b, vbNewLine).Count - 1
+            Dim line As String = Split(b, vbNewLine)(i).Replace(Chr(34), Chr(34) & " & System.Text.Encoding.ASCII.GetString(New Byte() {34}) & " & Chr(34))
+            ret &= Chr(34) & line & Chr(34) & " & System.Environment.NewLine & _" & vbNewLine
+        Next
+        Return ret.Remove(ret.Length - 6)
+    End Function
+    Sub ConvertBtE(ByVal Output As String)
+        UpdateStatus("Reading data...", 50)
+        Dim batch As String
+        If (Button3.Text = "From the textbox") Then
+            batch = IO.File.ReadAllText(TextBox1.Text)
+        Else
+            batch = TextBox3.Text
+        End If
+        UpdateStatus("Preparing code...", 25)
+        Dim code As String = Source
+        '        UpdateStatus("", 50)
+        code = code.Replace("{BATCH}", TobString(batch))
+        UpdateStatus("Compiling...", 50)
+        GenerateExecutable(Output, code)
+    End Sub
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If (Button3.Text = "From the textbox") And (Not IO.File.Exists(TextBox1.Text)) Then
+            MsgBox("Please select a valid batch file!", MsgBoxStyle.Critical, "Error")
+            Exit Sub
+        End If
+        Dim sfd As New SaveFileDialog
+        sfd.Filter = "Executables|*.exe"
+        If sfd.ShowDialog <> vbOK Then
+            Exit Sub
+        End If
+
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+        TextBox3.Enabled = False
+        TextBox4.Enabled = False
+
+        TextBox2.Text = sfd.FileName
+        Dim tConvert As New Threading.Thread(AddressOf ConvertBtE)
+        tConvert.Start(sfd.FileName)
+
+        If TextBox4.Text = "" Then
+            Dim msg As String
+            Dim title As String
+            Dim style As MsgBoxStyle
+            Dim response As MsgBoxResult
+            msg = "Do you want to add an Icon?"
+            title = "Add an Icon"
+            style = MsgBoxStyle.Question + MsgBoxStyle.YesNo
+            response = MsgBox(msg, style, title)
+            If response = MsgBoxResult.Yes Then
+
+
+                Dim ofd As New OpenFileDialog
+                ofd.Title = "Select an icon"
+                ofd.Filter = "Icon (*.ico)|*.ico"
+                If ofd.ShowDialog <> vbOK Then
+                    Exit Sub
+                End If
+                TextBox4.Text = ofd.FileName
+                PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+                PictureBox1.ImageLocation = ofd.FileName
+                ChangeIcon()
+                UpdateStatus("Injecting icon...", 75)
+            End If
+        End If
+        If TextBox4.Text = "" Then
+            ChangeIcon()
+            UpdateStatus("Injecting icon...", 75)
+        End If
+
+        UpdateStatus("Done", 100)
+
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        TextBox1.Enabled = True
+        TextBox2.Enabled = True
+        TextBox3.Enabled = True
+        TextBox4.Enabled = True
+
+    End Sub
+
+    Delegate Sub delUpdateStatus(ByVal lbl As String, ByVal prgs As Integer)
+    Sub UpdateStatus(ByVal lbl As String, ByVal prgs As Integer)
+        If InvokeRequired Then
+            Invoke(New delUpdateStatus(AddressOf UpdateStatus), New Object() {lbl, prgs})
+        Else
+            If lbl <> "" Then Label1.Text = "Status: " + lbl
+            ProgressBar1.Value = prgs
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Button3.Text = "Use text instead" Then
+            Button3.Text = "Use batch file"
+            Button1.Enabled = False
+            TextBox3.Enabled = True
+        Else
+            Button3.Text = "Use text instead"
+            Button1.Enabled = True
+            TextBox3.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim ofd As New OpenFileDialog
+        ofd.Title = "Select an icon"
+        ofd.Filter = "Icon (*.ico)|*.ico"
+        If ofd.ShowDialog <> vbOK Then
+            Exit Sub
+        End If
+        TextBox4.Text = ofd.FileName
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.ImageLocation = ofd.FileName
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        UpdateStatus("Inactive", 0)
+        Dim ofd As New OpenFileDialog
+        ofd.Filter = "Batch files|*.bat"
+        If ofd.ShowDialog <> vbOK Then
+            Exit Sub
+        End If
+        TextBox1.Text = ofd.FileName
+    End Sub
+
+    Private Sub closeApp_Click(sender As Object, e As EventArgs) Handles closeApp.Click
+        Close()
+    End Sub
+
+    Sub ChangeIcon()
+        fichier = TextBox2.Text
+        icone = TextBox4.Text
+        IconInjector.InjectIcon(fichier, icone)
+        MsgBox("Icon succesfully changed.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Information")
+    End Sub
+
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub Label6_MouseDown(sender As Object, e As MouseEventArgs) Handles Label6.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Label6_MouseMove(sender As Object, e As MouseEventArgs) Handles Label6.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Label6_MouseUp(sender As Object, e As MouseEventArgs) Handles Label6.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub PictureBox2_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub PictureBox2_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub KryptonLinkLabel1_LinkClicked(sender As Object, e As EventArgs) Handles KryptonLinkLabel1.LinkClicked
+        Readme.Show()
+    End Sub
+
+    Private Sub PictureBox2_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseUp
+        draggable = False
+    End Sub
+End Class
+
